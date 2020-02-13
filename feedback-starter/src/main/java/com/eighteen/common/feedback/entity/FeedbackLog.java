209cffa4cd5110c30aea307525e08b9ff84b9701
wangwei <wangwei@zhixueyun.com>
2020-02-13 22:04:51 +0800

    update

--- feedback-starter/src/main/java/com/eighteen/common/feedback/entity/FeedbackLog.java
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/entity/FeedbackLog.java
@@ -24,21 +24,36 @@ public class FeedbackLog {
     private Long id;
     @Column(name = "imei", columnDefinition = "varchar(50)")
     private String imei;
-    private String aid;
-    private String cid;
-    private String mac;
-    private String ip;
+    @Column(name = "imeiMd5", columnDefinition = "varchar(50)")
+    private String imeiMd5;
+    @Column(name = "androidId", columnDefinition = "varchar(50)")
+    private String androidId;
+    @Column(name = "androidMd5", columnDefinition = "varchar(50)")
+    private String androidMd5;
     private String oaid;
+    @Column(name = "oaidMd5", columnDefinition = "varchar(50)")
+    private String oaidMd5;
+    private String mac;
+    @Column(name = "ip", columnDefinition = "varchar(20)")
+    private String ip;
+    @Column(name = "mid", columnDefinition = "varchar(100)")
     private String mid;
     private Date ts;
-    private Integer channel;
+    @Column(name = "channel", columnDefinition = "varchar(15)")
+    private String channel;
+    @Column(name = "urlChannel", columnDefinition = "varchar(15)")
+    private String urlChannel;
     private Integer eventType;
-    private String androidId;
     private String MatchField;
     @Column(name = "callback_url", columnDefinition = "varchar(1500)")
     private String callbackUrl;
     private Integer coid;
     private Integer ncoid;
+
+    private String param1;
+    private String param2;
+    private String param3;
+    private String param4;
     private Date createTime;
 }
 
