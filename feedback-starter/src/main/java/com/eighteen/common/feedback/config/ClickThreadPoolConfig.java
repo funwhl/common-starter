52d900040eb3e802865a73a4785bd69d5e0a936f
wangwei <wangwei@zhixueyun.com>
2020-03-05 00:15:01 +0800

    update

---
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/config/ClickThreadPoolConfig.java
@@ -0,0 +1,32 @@
+package com.eighteen.common.feedback.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by wangwei.
+ * Date: 2020/3/4
+ * Time: 21:03
+ */
+public class ClickThreadPoolConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(8);
+        taskExecutor.setMaxPoolSize(16);
+        taskExecutor.setQueueCapacity(100);
+        taskExecutor.setThreadNamePrefix("click-ThreadPool");
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return null;
+    }
+}
