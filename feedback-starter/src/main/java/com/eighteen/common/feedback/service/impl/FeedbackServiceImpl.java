d1fef204daf1b3201cea2e428142d07f58a63b80
wangwei <wangwei@zhixueyun.com>
2020-03-09 07:26:51 +0800

    Revert "优化"
    This reverts commit 6c28d8a

--- feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
@@ -422,15 +422,9 @@ public class FeedbackServiceImpl implements FeedbackService, InitializingBean {
 
 
     @Override
-    public void syncActive(ShardingContext c){
+    public void syncActive(ShardingContext c) throws InterruptedException {
         int item = c.getShardingItem();
-        if(item>0) {
-            try {
-                Thread.sleep((item * 50L));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        if(item>0) Thread.sleep((item * 50L));
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         List<String> channel = Lists.newArrayList(etprop.getChannel());
         String types = etprop.getTypes();
