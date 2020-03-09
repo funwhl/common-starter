d116bee0ca7d9647d419cd43c65e8d98a354328a
wangwei <wangwei@zhixueyun.com>
2020-03-09 17:11:21 +0800

    Revert "优化"
    This reverts commit a72d384

--- feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
@@ -480,11 +480,9 @@ public class FeedbackServiceImpl implements FeedbackService, InitializingBean {
 
             data = data.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(o -> o.getIimei() + o.getCoid() + o.getNcoid()))), ArrayList::new));
             data = data.parallelStream().filter(o -> {
-                Boolean b = (StringUtils.isBlank(o.getImei()));
-                        if (b) return true;
                         Double score = redisTemplate.opsForZSet().score(getDayCacheRedisKey(String.format("active#imei#%d#%d", o.getCoid(), o.getNcoid())),
-                                String.format("%s%s%s", o.getImei(), o.getOaid(), o.getAndroidId()));
-                        return ((active == null || !active.contains(o))
+                                o.getImei());
+                        return (StringUtils.isBlank(o.getImei()))||((active == null || !active.contains(o))
                                 //                    &&!countHistory(new DayHistory().setWd("imei").setValue(o.getImei()).setCoid(o.getCoid()).setNcoid(o.getNcoid()))
                                 && (score == null || score <= 0));
                     }
@@ -526,7 +524,7 @@ public class FeedbackServiceImpl implements FeedbackService, InitializingBean {
                     data.parallelStream().forEach(a -> {
                         ActiveLogger log = activeLoggerDao.save(a);
                         if (StringUtils.isNotBlank(a.getImei()))
-                            redisTemplate.opsForZSet().add(getDayCacheRedisKey(String.format("active#imei#%d#%d", a.getCoid(), a.getNcoid())), String.format("%s%s%s", log.getImei(), log.getOaid(), log.getAndroidId()), a.getId().doubleValue());
+                            redisTemplate.opsForZSet().add(getDayCacheRedisKey(String.format("active#imei#%d#%d", a.getCoid(), a.getNcoid())), log.getImei(), a.getId().doubleValue());
 
 //                            redis.zadd(getDayCacheRedisKey("active#imei#" + a.getCoid() + "#" + a.getNcoid()), log.getId().doubleValue(), a.getImeiMd5());
 //                        if (StringUtils.isNotBlank(a.getOaidMd5()))
