9854d0a344fc341a046ed0a7099c09733d28e74d
wangwei <wangwei@zhixueyun.com>
2020-03-13 06:49:14 +0800

    test

--- feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/service/impl/FeedbackServiceImpl.java
@@ -802,9 +802,14 @@ public class FeedbackServiceImpl implements FeedbackService, InitializingBean {
                 String redisKey = getDayCacheRedisKey(key);
 //                Map<String, Double> map = new HashMap<>(dayHistories.size());
 //                dayHistories.forEach(dayHistory -> map.put(String.format("%d##%d##%s", dayHistory.getCoid(), dayHistory.getNcoid(), dayHistory.getValue()), (double) dayHistory.getCreateTime().getTime()));
-                Long count = redisTemplate.opsForZSet().add(redisKey, dayHistories.stream().map(o -> new DefaultTypedTuple<Object>(String.format("%d##%d##%s", o.getCoid(), o.getNcoid(), o.getValue()), (double) o.getCreateTime().getTime())).collect(Collectors.toSet()));
-                DayHistory history = dayHistories.get(0);
-                log.info("step addcache : ret {},{},{}",count, history.toString(),history.getCreateTime().getTime());
+                dayHistories.parallelStream().forEach(dayHistory -> {
+                    Boolean count = redisTemplate.opsForZSet().add(redisKey,String.format("%d##%d##%s", dayHistory.getCoid(), dayHistory.getNcoid(), dayHistory.getValue()),(double) dayHistory.getCreateTime().getTime());
+                log.info("step addcache : ret {},{},{}",count, dayHistory.toString(),dayHistory.getCreateTime().getTime());
+
+                });
+//                Long count = redisTemplate.opsForZSet().add(redisKey, dayHistories.stream().map(o -> new DefaultTypedTuple<Object>(String.format("%d##%d##%s", o.getCoid(), o.getNcoid(), o.getValue()), (double) o.getCreateTime().getTime())).collect(Collectors.toSet()));
+//                DayHistory history = dayHistories.get(0);
+//                log.info("step addcache : ret {},{},{}",count, history.toString(),history.getCreateTime().getTime());
 
 //                redis.process(j -> j.zadd(redisKey, map));
             } else {
