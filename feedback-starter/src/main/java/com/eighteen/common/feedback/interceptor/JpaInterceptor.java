74e04875f74f656fe9d775655cdfe8949c6a1e23
wangwei <wangwei@zhixueyun.com>
2020-03-24 04:30:18 +0800

    优化回传查询sql

---
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/interceptor/JpaInterceptor.java
@@ -0,0 +1,17 @@
+package com.eighteen.common.feedback.interceptor;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+
+/**
+ * Created by wangwei.
+ * Date: 2020/3/24
+ * Time: 3:50
+ */
+public class JpaInterceptor implements StatementInspector {
+    @Override
+    public String inspect(String sql) {
+        if (sql.contains("inner join t_click_log"))
+            sql = sql.replace("t_active_logger activelogg0_ inner join t_click_log clicklog1_", "t_active_logger activelogg0_ with(nolock,index(activeTime)) inner join t_click_log clicklog1_ with(nolock,index(clickTime))");
+        return sql;
+    }
+}
