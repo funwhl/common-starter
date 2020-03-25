d3a46059f1be57e20723733889f30f46003ead32
wangwei <wangwei@zhixueyun.com>
2020-03-25 19:44:17 +0800

    修改清理job执行时间

--- feedback-starter/src/main/java/com/eighteen/common/feedback/EighteenProperties.java
+++ feedback-starter/src/main/java/com/eighteen/common/feedback/EighteenProperties.java
@@ -26,12 +26,12 @@ public class EighteenProperties {
     private Integer preFetch = 1000;
     private Integer preFetchActive = 5000;
     //job
-    private String cleanClickCron = "0 10/30 * * * ?";
+    private String cleanClickCron = "0 10 04 * * ?";
     private String cleanImeiCron = "0 0 0/1 * * ?";
     private String feedbackCron = "0 1/1 * * * ?";
     private String feedbackColdCron;
     private String syncActiveCron = "0 0/1 * * * ?";
-    private String cleanActiveCron = "0 15/30 * * * ?";
+    private String cleanActiveCron = "0 25 04 * * ?";
     private String cleanActiveHistoryCron = "0 7/10 * * * ?";
     private String dayStatCron = "0 0 1 1/1 * ?";
     private String retentionCron = "0 0 0/1 * * ?";
