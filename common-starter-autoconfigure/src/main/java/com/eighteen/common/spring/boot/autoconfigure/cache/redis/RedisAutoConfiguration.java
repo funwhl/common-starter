4d1c0d6da95dceeadcfbf08cb8904db93ccadebb
louchen <louchen@18.net>
2020-04-22 09:41:02 +0800

    统一redisTemplate的序列化

--- common-starter-autoconfigure/src/main/java/com/eighteen/common/spring/boot/autoconfigure/cache/redis/RedisAutoConfiguration.java
+++ common-starter-autoconfigure/src/main/java/com/eighteen/common/spring/boot/autoconfigure/cache/redis/RedisAutoConfiguration.java
@@ -5,6 +5,7 @@ import com.eighteen.common.serializer.hessian.HessianSerializer;
 import com.eighteen.common.serializer.Serializer;
 import com.eighteen.common.spring.boot.autoconfigure.cache.Cache;
 import com.eighteen.common.spring.boot.autoconfigure.cache.CacheService;
+import com.eighteen.common.spring.boot.autoconfigure.redis.RedisTemplateHelper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -103,14 +104,7 @@ public class RedisAutoConfiguration {
     public RedisTemplate<Object, Object> redisTemplate(
             RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
-        //使用fastjson序列化
-        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
-        // value值的序列化采用fastJsonRedisSerializer
-        template.setValueSerializer(fastJsonRedisSerializer);
-        template.setHashValueSerializer(fastJsonRedisSerializer);
-        // key的序列化采用StringRedisSerializer
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
+        RedisTemplateHelper.setSerializer(template);
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
