f9b2bbf1a1f34869f202e50075f001ce1909fe50
wangwei <wangwei@18.net>
2020-08-06 09:10:51 +0800

    0 channel点击未找到

--- feedback-data/src/main/java/com/eighteen/common/feedback/domain/ErrorCode.java
+++ feedback-data/src/main/java/com/eighteen/common/feedback/domain/ErrorCode.java
@@ -11,8 +11,9 @@ public enum ErrorCode {
     first_link_time_out_range(-1, "first link time is out of range not in [today ,1970 ,null]"),
     um_score_checked_failed(100, "um score is less than the config score"),
     call_callback_url_error(101, "call callbackUrl api error"),
-    click_channel_unset(101, "click channel unset"),
-    channel_type_unset(101, "channel type unset");
+    click_channel_unset(102, "click channel unset"),
+    channel_type_unset(103, "channel type unset"),
+    can_not_find_click_log(104, "can not find click");
     Integer code;
     String message;
 
